068d02ab602c3dd76a3895b6510d1cc4d106cfdb
zhouhanjiang1987 <zhouhanjiang1987@gmail.com>
2019-11-27 16:07:16 +0800

    Update ArraySlice.py

--- ArraySlice.py
+++ ArraySlice.py
@@ -5,13 +5,13 @@
 from __future__ import division
 
 '''
-__Author__:沂水寒城
+__Author__:zhouhanjiang
 学习Python中的X[:,0]、X[:,1]、X[:,:,0]、X[:,:,1]、X[:,m:n]和X[:,:,m:n]
 '''
 
 import numpy as np
 
-def simple_test():
+def Array_Slice_Test():
     '''
     简单的小实验
     '''
@@ -69,7 +69,21 @@ def simple_test():
     print 'X[:,:,m:n]结果输出为：'
     print data_list[:,:,0:1] #[ [[ 1][ 1][ 3][ 7][ 4]] ...]
     print data_list[:,:,0:2] #[[[  1   2][  1   0][  3   4][  7   9][  4   0]]...]
+    print 'X[m,:,:]结果输出为：'
+    print data_list[0,:,:] #[[1 2][1 0][3 4][7 9][4 0]]
+    print data_list[0,1,:] #[1 0]
+    print data_list[0,1,0] #[1 ]
+    print data_list[1,:,:] #[[1 4][1 5][3 6][8 9][5 0]]
+    print data_list[1,1,:] #[1 5]
+    print data_list[1,1,1] #[ 5]
+    print 'X[:,m,:]结果输出为：'
+    print data_list[:,0,:] #[[ 1  2][ 1  4] [ 8  2][ 1  1][ 9  2][ 8  2][ 1 22][ 1  5][11  2]]
+    print data_list[:,0,0] #[ 1  1  8  1  9  8  1  1 11]
+    print data_list[:,0,1] #[ 2  4  2  1  2  2 22  5  2]
+    print data_list[:,1,:] #[[  1   0][  1   5][  1   8][  1   2][  1   3][  1   9][  1   2][  1  20][  1 110]]
+    print data_list[:,1,0] #[1 1 1 1 1 1 1 1 1]
+    print data_list[:,1,1] #[  0   5   8   2   3   9   2  20 110]
 
 
 if __name__ == '__main__':
-    simple_test()
+    Array_Slice_Test()
