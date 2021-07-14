217668ea23ebf621ef3be76d7906c661f2dc2053
zhouhanjiang <zhouhanjiang@xunlei.com>
2021-07-14 15:25:56 +0800

    装饰器函数优化

--- Decorator/Decorator.py
+++ Decorator/Decorator.py
@@ -1,6 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Filename: decorator.py
+# Author: zhouhanjiang@xunlei.com
+# Create: 2021/07/14 10:03
+# LastMod: 2021/07/14 10:03
 
+"""
+    decorator.py
+"""
 
 from __future__ import division
 
@@ -12,15 +19,16 @@ __Author__:zhouhanjiang
 '''
 
 import os
-import time
+# import time
 
 from Utils.logger import get_logger
 
 logger = get_logger(
-        os.path.basename(os.path.abspath(__file__)),
-        os.path.abspath(__file__),
-        level="INFO",
-        log_file_max_bytes=1024 * 1024)
+    os.path.basename(os.path.abspath(__file__)),
+    os.path.abspath(__file__),
+    level="INFO",
+    log_file_max_bytes=1024 * 1024)
+
 
 # import builtins as __builtin__
 
@@ -39,109 +47,144 @@ def print(*args, **kwargs):
     return logger.info(*args, **kwargs)
 
 
-#------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # an example of multiple python decorator
-def deco1(func):
-    print("step1.deco1 start")
-    print("in deco1.func.__name__=" + str(func.__name__))
+def deco1_with_wrapper(func):
+    """
+       deco1_with_wrapper
+    """
+    print("step1.deco1_with_wrapper start")
+    print("in deco1_with_wrapper.func.__name__=" + str(func.__name__))
+
     @wraps(func)
-    def wrapper1(*args,**kwargs):
-        print("step2.wrapper1 start")
-        print("in wrapper1.func.__name__=" + str(func.__name__))
-        print("in wrapper1.args=" + str(args) + ";*kwargs=" + str(*kwargs))
+    def deco_with_wrapper_innder(*args, **kwargs):
+        """
+           deco_with_wrapper_innder
+        """
+        print("step2.deco_with_wrapper_innder start")
+        print("in deco_with_wrapper_innder.func.__name__=" + str(func.__name__))
+        print("in deco_with_wrapper_innder.args=" + str(args) + ";*kwargs=" + str(*kwargs))
         result1 = func(*args, **kwargs)
-        print("in wrapper1.result=" + str(result1))
-        print("step3.wrapper1 end")
+        print("in deco_with_wrapper_innder.result=" + str(result1))
+        print("step3.deco_with_wrapper_innder end")
         return result1
-    print("step4.deco1 end")
-    return wrapper1
 
-def deco2(func):
-    print("step5.deco2 start")
-    print("in deco2.func.__name__=" + str(func.__name__))
-    #@wraps(func)
-    def wrapper2(*args,**kwargs):
-        print("step6.wrapper2 start")
-        print("in wrapper2.func.__name__=" + str(func.__name__))
-        print("in wrapper2.args=" + str(args) + ";*kwargs=" + str(*kwargs))
-        result2 = func(*args,**kwargs)
-        print("in wrapper2.result=" + str(result2))
-        print("step7.wrapper2 end")
+    print("step4.deco1_with_wrapper end")
+    return deco_with_wrapper_innder
+
+
+def deco2_without_wrapper(func):
+    """
+        deco2_without_wrapper
+    """
+    print("step5.deco2_without_wrapper start")
+    print("in deco2_without_wrapper.func.__name__=" + str(func.__name__))
+
+    # @wraps(func)
+    def deco_without_wrapper_innder(*args, **kwargs):
+        """
+          deco_without_wrapper_innder
+        """
+        print("step6.deco_without_wrapper_innder start")
+        print("in deco_without_wrapper_innder.func.__name__=" + str(func.__name__))
+        print("in deco_without_wrapper_innder.args=" + str(args) + ";*kwargs=" + str(*kwargs))
+        result2 = func(*args, **kwargs)
+        print("in deco_without_wrapper_innder.result=" + str(result2))
+        print("step7.deco_without_wrapper_innder end")
         return result2
-    print("step8.deco2 end")
-    return wrapper2
 
-def deco3_with_para(deco_para="deco3_para"):
-    print("step9.deco3_with_para start")
-    print("in deco3_with_para.deco_para=" + str(deco_para))
-    def deco3(func):
-      print("in deco3.func.__name__=" + str(func.__name__))
-      #@wraps(func)
-      def wrapper3(*args,**kwargs):
-        print("step10.wrapper3 start")
-        print("in wrapper3.func.__name__=" + str(func.__name__))
-        print("in wrapper3.deco_para=" + str(deco_para))
-        print("in wrapper3.args="+str(args)+";*kwargs="+str(*kwargs))
-        result3 = func(*args,**kwargs)
-        print("in wrapper3.result="+str(result3))
-        print("step11.wrapper3 end")
-        return result3
-      return wrapper3
-
-    print("step12.deco3_with_para end")
-    return deco3
+    print("step8.deco2_without_wrapper end")
+    return deco_without_wrapper_innder
 
 
-@deco3_with_para("deco3_para_test")
-@deco1
-@deco2
-def multi_deco_test_with_return(*args,**kwargs):
+def deco3_para_without_wrapper(deco_para="deco3_para"):
+    """
+        deco3_para_without_wrapper
+    """
+    print("step9.deco3_para_without_wrapper start")
+    print("in deco3_para_without_wrapper.deco_para=" + str(deco_para))
+
+    def deco3_innder(func):
+        """
+           deco3_innder
+        """
+        print("in deco3_innder.func.__name__=" + str(func.__name__))
+
+        # @wraps(func)
+        def deco3_para_without_wrapper_innder(*args, **kwargs):
+            """
+                deco3_para_without_wrapper_innder
+            """
+            print("step10.deco3_para_without_wrapper_innder start")
+            print("in deco3_para_without_wrapper_innder.func.__name__=" + str(func.__name__))
+            print("in deco3_para_without_wrapper_innder.deco_para=" + str(deco_para))
+            print("in deco3_para_without_wrapper_innder.args=" + str(args) + ";*kwargs=" + str(*kwargs))
+            result3 = func(*args, **kwargs)
+            print("in deco3_para_without_wrapper_innder.result=" + str(result3))
+            print("step11.deco3_para_without_wrapper_innder end")
+            return result3
+
+        return deco3_para_without_wrapper_innder
+
+    print("step12.deco3_para_without_wrapper end")
+    return deco3_innder
+
+
+@deco3_para_without_wrapper("deco3_para_test")
+@deco1_with_wrapper
+@deco2_without_wrapper
+def multi_deco_test_with_return(*args, **kwargs):
+    """
+       multi_deco_test_with_return
+    """
     print('multi_deco_test called')
     return args
 
 
-#multi_deco_test_result = multi_deco_test_with_return("args_para1","args_para2",kwargs_key="kwargs_value")
-#print("multi_deco_test_result="+str(multi_deco_test_result))
-'''
-output:
-
-step9.deco3_with_para start
-in deco3_with_para.deco_para=deco3_para_test
-step12.deco3_with_para end
-step5.deco2 start
-in deco2.func.__name__=multi_deco_test_with_return
-step8.deco2 end
-step1.deco1 start
-in deco1.func.__name__=wrapper2
-step4.deco1 end
-in deco3.func.__name__=wrapper2
-step10.wrapper3 start
-in wrapper3.func.__name__=wrapper2
-in wrapper3.deco_para=deco3_para_test
-in wrapper3.args=('args_para1', 'args_para2');*kwargs=kwargs_key
-step2.wrapper1 start
-in wrapper1.func.__name__=wrapper2
-in wrapper1.args=('args_para1', 'args_para2');*kwargs=kwargs_key
-step6.wrapper2 start
-in wrapper2.func.__name__=multi_deco_test_with_return
-in wrapper2.args=('args_para1', 'args_para2');*kwargs=kwargs_key
-multi_deco_test called
-in wrapper2.result=('args_para1', 'args_para2')
-step7.wrapper2 end
-in wrapper1.result=('args_para1', 'args_para2')
-step3.wrapper1 end
-in wrapper3.result=('args_para1', 'args_para2')
-step11.wrapper3 end
-multi_deco_test_result=('args_para1', 'args_para2')
-'''
-#------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 
-def Decorator_Test():
-  pass
+def decorator_test():
+    """
+        decorator_test
+    """
+    multi_deco_test_result = multi_deco_test_with_return("args_para1", "args_para2", kwargs_key="kwargs_value")
+    print("multi_deco_test_result=" + str(multi_deco_test_result))
+    '''
+    output:
+
+    step9.deco3_para_without_wrapper start
+    in deco3_para_without_wrapper.deco_para=deco3_para_test
+    step12.deco3_para_without_wrapper end
+    step5.deco2_without_wrapper start
+    in deco2_without_wrapper.func.__name__=multi_deco_test_with_return
+    step8.deco2_without_wrapper end
+    step1.deco1_with_wrapper start
+    in deco1_with_wrapper.func.__name__=deco_without_wrapper_innder
+    step4.deco1_with_wrapper end
+    in deco3.func.__name__=deco_without_wrapper_innder
+    step10.deco3_para_without_wrapper_innder start
+    in deco3_para_without_wrapper_innder.func.__name__=deco_without_wrapper_innder
+    in deco3_para_without_wrapper_innder.deco_para=deco3_para_test
+    in deco3_para_without_wrapper_innder.args=('args_para1', 'args_para2');*kwargs=kwargs_key
+    step2.deco_with_wrapper_innder start
+    in deco_with_wrapper_innder.func.__name__=deco_without_wrapper_innder
+    in deco_with_wrapper_innder.args=('args_para1', 'args_para2');*kwargs=kwargs_key
+    step6.deco_without_wrapper_innder start
+    in deco_without_wrapper_innder.func.__name__=multi_deco_test_with_return
+    in deco_without_wrapper_innder.args=('args_para1', 'args_para2');*kwargs=kwargs_key
+    multi_deco_test called
+    in deco_without_wrapper_innder.result=('args_para1', 'args_para2')
+    step7.deco_without_wrapper_innder end
+    in deco_with_wrapper_innder.result=('args_para1', 'args_para2')
+    step3.deco_with_wrapper_innder end
+    in deco3_para_without_wrapper_innder.result=('args_para1', 'args_para2')
+    step11.deco3_para_without_wrapper_innder end
+    multi_deco_test_result=('args_para1', 'args_para2')
+    '''
 
 
 if __name__ == '__main__':
-    Decorator_Test()
+    decorator_test()
